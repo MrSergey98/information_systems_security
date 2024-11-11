eefbdd9edad9d60155bafd2ff7d3061499462bd7
MrSergey98 <112868520+MrSergey98@users.noreply.github.com>
2024-11-11 17:38:44 +0300

    feat(LR3): add one switch encoding method

--- LR3.py
+++ LR3.py
@@ -3,6 +3,21 @@ from tkinter import filedialog, messagebox
 
 alphabet: str = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ,.!?*+=—-1234567890;:'	()\"`\n"
 
+def getCipher(origin_key, origin_text):
+    clear_text = ''.join(origin_text.split(' ')).lower()
+    k = len(clear_text) // len(origin_key)
+
+    cipher = {}
+    for index, ch in enumerate(origin_key.lower()):
+        if ch in cipher:
+            cipher[ch] += clear_text[index * k : index * k + k]
+        else:
+            cipher[ch] = clear_text[index * k : index * k + k]
+
+    cipher_text = ''.join([''.join([cipher[key][index] for key in sorted(cipher.keys())]) for index in range(k)])
+    return ' '.join([cipher_text[index : index + k] for index in range(0, len(cipher_text), k)]).upper()
+
+
 # Функция шифрования текста
 def encode(text: str, a: int = 1, b: int = 1, c: int = 1) -> str:
     decode: str = ""
